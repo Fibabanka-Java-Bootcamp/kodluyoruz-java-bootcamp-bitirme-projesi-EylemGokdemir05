659cd84796276e487293812ad5a4ce0de6bcfe20
EylemGokdemir05 <41906907+EylemGokdemir05@users.noreply.github.com>
2021-02-13 23:56:04 +0300

    first test added.

--- src/test/java/org/kodluyoruz/mybank/MybankApplicationTests.java
+++ src/test/java/org/kodluyoruz/mybank/MybankApplicationTests.java
@@ -1,13 +1,16 @@
 package org.kodluyoruz.mybank;
 
 import org.junit.jupiter.api.Test;
+import org.kodluyoruz.mybank.card.Card;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MybankApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testBoundaryEqual() {
+        Card card=new Card();
+        assertEquals(2000,card.getBoundary());
     }
-
 }
